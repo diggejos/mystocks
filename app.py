9c8f0b755315a4963db18626d989b32b262c8c48
diggejos <101020608+diggejos@users.noreply.github.com>
2024-09-02 10:36:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2170,6 +2170,13 @@ app.index_string = '''
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T6SPT9FD');</script>
+        <!-- End Google Tag Manager -->
         {%metas%}
         <title>Stocks monitoring and recommendation made easy - MyStocks</title>
         <meta name="description" content="Track and forecast stock prices, visualize trends, get stock recommendations, and chat with an AI financial advisor. Save your watchlist today!">
@@ -2197,6 +2204,10 @@ app.index_string = '''
         </style>
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6SPT9FD"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         {%app_entry%}
         <footer>
             {%config%}
