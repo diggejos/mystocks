22b7705fc246b3a025990d862af967ede107d6fa
Josua <101020608+diggejos@users.noreply.github.com>
2024-09-26 20:28:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1687,7 +1687,7 @@ app.index_string = '''
         <meta property="og:url" content="https://mystocksportfolio.io/" />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="canonical" href="https://mystocksportfolio.io/" />
-        <link rel="icon" href="/assets/logo_with_transparent_background.png" type="image/x-icon">
+        <link rel="icon" href="/assets/logo_with_transparent_background_favicon.png" type="image/png">
 
         {%favicon%}
         {%css%}
