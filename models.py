1ffef203ea123311b011b426747f76b5ec9e3f2e
Josua <101020608+diggejos@users.noreply.github.com>
2024-10-06 20:52:59 +0200

    Update models.py

--- models.py
+++ models.py
@@ -9,12 +9,12 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=True)
+    google_id = db.Column(db.String(200), unique=True, nullable=True)  # Store Google ID
     theme = db.Column(db.String(50), nullable=True)  # New field for storing the theme
     watchlists = db.relationship('Watchlist', backref='user', lazy=True)
     confirmed = db.Column(db.Boolean, default=False)
@@ -22,6 +22,12 @@ class User(db.Model):
     subscription_status = db.Column(db.String(50), default="inactive")  # Track subscription status
     payment_status = db.Column(db.Boolean, default=False)  # Added field for payment status
     stripe_subscription_id = db.Column(db.String(100), nullable=True)  # Add stripe_subscription_id
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Watchlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
