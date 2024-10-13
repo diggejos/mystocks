1d702e333770da1ba87994b437871be96d14eefb
Josua <101020608+diggejos@users.noreply.github.com>
2024-10-13 21:04:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1517,6 +1517,7 @@ app.index_string = '''
         <link rel="preload" href="https://cdn.jsdelivr.net/.../bootstrap.min.css" as="style">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/.../bootstrap.min.css">
         <meta charset="UTF-8">
+        <meta name="yandex-verification" content="234fe899dd410f3e" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Track and forecast your favorite stocks, visualize trends, get stocks recommendations, and chat with an AI financial advisor. Save your watchlist today!">
         <meta name="keywords" content="stock, stocks, stocks dashboard, finance, stocks forecasting, stocks news, stocks monitoring, stocks recommendations, finance, financial advisor, watchlist, watchmystocks, mystocks, apple stock, microsoft stock">
