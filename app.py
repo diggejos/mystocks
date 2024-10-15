17932e9eb7dbe873bbc01ca030e0fff9b2d9b447
Josua <101020608+diggejos@users.noreply.github.com>
2024-10-15 15:56:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ server = app.server
 
 #Minify(app=server, html=True, js=True, cssless=True)
 
-app.server.config['COMPRESS_LEVEL'] = 8  # Set compression level (1-9, default is 6)
+app.server.config['COMPRESS_LEVEL'] = 9  # Set compression level (1-9, default is 6)
 app.server.config['COMPRESS_MIN_SIZE'] = 500  # Minimum size (in bytes) to trigger compression
 app.server.config['COMPRESS_MIMETYPES'] = ['text/html', 'text/css', 'application/javascript']  # File types to compress
 compress = Compress()
