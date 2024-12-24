4c21df6963560d2d7e9478350134e5f233a9a09c
Josua <101020608+diggejos@users.noreply.github.com>
2024-12-24 15:28:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1442,6 +1442,33 @@ app.index_string = '''
         }
         </script>
 
+        <!-- Lazy-loaded third-party scripts -->
+        <script>
+            window.addEventListener('load', function () {
+                // Google Tag Manager
+                (function(w, d, s, l, i) {
+                    w[l] = w[l] || [];
+                    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                    var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true;
+                    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-T6SPT9FD');
+
+                // Hotjar
+                (function(h, o, t, j, a, r) {
+                    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+                    h._hjSettings = { hjid: 5148807, hjsv: 6 };
+                    a = o.getElementsByTagName('head')[0];
+                    r = o.createElement('script');
+                    r.async = 1;
+                    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+            });
+        </script>
 
         <!-- Defer Dash scripts -->
         <script defer src="https://cdnjs.cloudflare.com/ajax/libs/dash/1.16.3/dash.min.js"></script>
